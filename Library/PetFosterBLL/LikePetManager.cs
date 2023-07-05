6fa46509c4b2c5374dbb4560f0588064f3fea947
Mr.Meng <3420575883@qq.com>
2023-07-05 11:41:35 +0800

    完成了ForumPostManager的事务逻辑

--- Library/PetFosterBLL/LikePetManager.cs
+++ Library/PetFosterBLL/LikePetManager.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using PetFoster.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PetFoster.BLL
 {
-    internal class LikePetManager
+    public class LikePetManager
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Limitrow"></param>
+        /// <param name="Orderby"></param>
+        public static void ShowLikePet(int Limitrow = -1, string Orderby = null)
+        {
+            DataTable dt = LikePetServer.LikePetInfo(Limitrow, Orderby);
+            //调试用
+            foreach (DataColumn column in dt.Columns)
+            {
+                Console.Write("{0,-15}", column.ColumnName);
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write("{0,-15}", item);
+                }
+                Console.WriteLine();
+            }
+        }
     }
 }
