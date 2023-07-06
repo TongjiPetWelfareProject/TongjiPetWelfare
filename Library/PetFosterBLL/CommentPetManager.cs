f792ef109c0c32e4a09aa0ea5d44b5b5aff8efc1
Mr.Meng <3420575883@qq.com>
2023-07-06 11:39:42 +0800

    CommentPetServer中的Delete存在问题

--- Library/PetFosterBLL/CommentPetManager.cs
+++ Library/PetFosterBLL/CommentPetManager.cs
@@ -10,9 +10,9 @@ namespace PetFoster.BLL
 {
     public class CommentPetManager
     {
-        public static void ShowCommentPet(int Limitrow = -1, string Orderby = null)
+        public static void ShowCommentPet(int Limitrow = -1, string Orderby = null, string UID = "-1", string PID = "-1")
         {
-            DataTable dt = CommentPetServer.CommentPetInfo(Limitrow, Orderby);
+            DataTable dt = CommentPetServer.CommentPetInfo(Limitrow, Orderby, UID, PID);
             //调试用
             foreach (DataColumn column in dt.Columns)
             {
@@ -28,6 +28,18 @@ namespace PetFoster.BLL
                 }
                 Console.WriteLine();
             }
+        }
+        public static void GiveAComment(string UID, string PID,string content)
+        {
+            //调试用
+            CommentPetServer.InsertCommentPet(UID, PID,content);
+            Console.WriteLine($"{UID} gives a comment that reads {content} to {PID}."); // 输出点赞信息
+        }
+        public static void UndoAComment(string UID, string PID,DateTime dateTime)
+        {
+            //调试用
+            CommentPetServer.DeleteCommentPet(UID, PID, dateTime);
+            Console.WriteLine($"{UID} undo a like to {PID}."); // 输出点赞信息
         }
     }
 }
