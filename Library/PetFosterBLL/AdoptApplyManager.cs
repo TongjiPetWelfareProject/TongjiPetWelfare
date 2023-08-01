1f1aa34c178260b727f9ea662e1dd189fedddb73
VictorHuu <victorhu493@gmail.com>
2023-08-01 19:35:52 +0800

    领养申请表页面与审核实现 #5 in progress

---
+++ Library/PetFosterBLL/AdoptApplyManager.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PetFoster.DAL;
+namespace PetFoster.BLL
+{
+    public class AdoptApplyManager
+    {
+        public static void ShowCensorAdopt(int censorstate = 0, int Limitrow = -1, string Orderby = null)
+        {
+            string censorStr = JsonHelper.GetErrorMessage("censor_state", censorstate);
+
+            DataTable dt =AdoptApplyServer.FosterInfo(censorStr, Limitrow, Orderby);
+            //调试用
+            foreach (DataColumn column in dt.Columns)
+            {
+                Console.Write("{0,-15}", column.ColumnName);
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write("{0,-15}", item);
+                }
+                Console.WriteLine();
+            }
+        }
+        public static int ApplyAdopt(string UID, bool gender, bool pet_exp, bool long_term_care,
+            bool w_to_treat, decimal d_care_h, string P_Caregiver, decimal f_popul, bool be_children, bool accept_vis)
+        {
+            AdoptApplyServer.InsertAdoptApply(UID,gender,pet_exp,long_term_care,w_to_treat,
+            d_care_h, P_Caregiver,f_popul,be_children,accept_vis);
+            return 0;
+        }
+        //审核通过并随机分配一个有空的宠物
+        public static int CensorAdopt(string UID,out int errcode)
+        {
+            //1.随机选择一个宠物（不被占用）
+            int pid = AdoptApplyServer.GetRandomPet();
+            if (pid == -1)
+            {
+                Console.WriteLine($"宠物已经全被领养或寄养（正在申请寄养）走了!");
+                errcode = 1;//如上
+                return -1;
+            }else if (pid == -2)
+            {
+                Console.WriteLine($"审核过程异常!");
+                errcode = 2;
+                return -1;
+            }
+            //2.将此宠物交给主人领养
+            int err = 0;
+            AdoptServer.InsertAdopt(UID, pid.ToString(),out err);
+            if (err == -1)
+            {
+                Console.WriteLine($"不存在该申请人!");
+                errcode = 3;
+                return -1;
+            }
+            //3.审核状态为通过
+            AdoptApplyServer.UpdateAdoptEntry(UID, "legitimate");
+            errcode = 0;//正常运行
+            return pid;
+        }
+    }
+}
--- Library/PetFosterDAL/AdoptServer.cs
+++ Library/PetFosterDAL/AdoptServer.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,42 @@ namespace PetFoster.DAL
 {
     public class AdoptServer
     {
+        public static string user = "\"C##PET\"";
+        public static string pwd = "campus";
+        public static string db = "localhost:1521/orcl";
+        private static string conStr = "User Id=" + user + ";Password=" + pwd + ";Data Source=" + db + ";"; // 替换为实际的数据库连接字符串
+        public static void InsertAdopt(string UID,string PID,out int errcode)
+        {
+            // 添加新行
+            try
+            {
+                using (OracleConnection connection = new OracleConnection(conStr))
+                {
+                    connection.Open();
+                    OracleCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "INSERT INTO adopt (adopter_id, pet_id)" +
+                    $"VALUES ({UID},{PID})";
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                        errcode = 0;
+                        Console.WriteLine($"{UID}申请成功，被分配到的宠物是{PID}");
+                    }
+                    catch (OracleException ex)
+                    {
+                        errcode = -1;
+                        Console.WriteLine("不存在的用户或宠物");
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                // 处理异常
+                errcode= -2;
+                Console.WriteLine(ex.ToString());
+            }
+        }
     }
 }
