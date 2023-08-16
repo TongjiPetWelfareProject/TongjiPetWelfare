66e3bcbbc6f6fefc30c848f81454c42642e104f2
VictorHuu <victorhu493@gmail.com>
2023-08-16 23:37:24 +0800

    增加了预约功能

--- Glue/PetFosterBLL/AppointmentManager.cs
+++ Glue/PetFosterBLL/AppointmentManager.cs
@@ -1,18 +1,22 @@
-﻿using PetFoster.DAL;
+﻿using Oracle.ManagedDataAccess.Client;
+using PetFoster.DAL;
+using PetFoster.Model;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Drawing.Drawing2D;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PetFoster.BLL
 {
-    public class ApplicationManager
+    public class AppointmentManager
     {
         public static void ShowApplies(string UID=null,string PID=null,string Categories=null)
         {
-            DataTable dt = ApplicationServer.ApplyInfo(UID,PID,Categories);
+            DataTable dt = AppointmentServer.ApplyInfo(UID,PID,Categories);
             //调试用
             foreach (DataColumn column in dt.Columns)
             {
@@ -28,6 +32,13 @@ namespace PetFoster.BLL
                 }
                 Console.WriteLine();
             }
+        }
+        public void Appointment(string UID,string Pname,string species,DateTime dt,string reason)
+        {
+            //1.注册宠物信息
+            string PID = PetFoster.DAL.PetServer.InsertPet(Pname, species, "medium", DateTime.MinValue);
+            //2.挂号
+            AppointmentServer.InsertAppointment(UID,PID, dt, reason);
         }
     }
 }
