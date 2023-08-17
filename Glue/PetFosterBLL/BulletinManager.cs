a44ab78f7a1b3b0599a381a63cb005f92fe0f8d8
VictorHuu <victorhu493@gmail.com>
2023-08-17 11:06:46 +0800

    增加了搜索公告与显示公告的功能

--- Glue/PetFosterBLL/BulletinManager.cs
+++ Glue/PetFosterBLL/BulletinManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PetFoster.DAL;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,30 @@ namespace PetFoster.BLL
 {
     public class BulletinManager
     {
+        //搜索公告 最新
+        public static void ShowBulletins(string text)
+        {
+            DataTable dt = BulletinServer.BulletinInfo(text);
+            //调试用
+            foreach (DataColumn column in dt.Columns)
+            {
+                Console.Write("{0,-20}", column.ColumnName);
+            }
+            Console.WriteLine();
+
+            foreach (DataRow row in dt.Rows)
+            {
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write("{0,-20}", item);
+                }
+                Console.WriteLine();
+            }
+        }
+        //公告详细界面
+        public static void ShowBulletinVerbose(string BID)
+        {
+            PetFoster.Model.Bulletin target = BulletinServer.SelectBulletin(BID);
+        }
     }
 }
--- Glue/PetFosterDAL/BulletinServer.cs
+++ Glue/PetFosterDAL/BulletinServer.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Oracle.ManagedDataAccess.Client;
+using PetFoster.Model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +13,78 @@ namespace PetFoster.DAL
     public class BulletinServer
     {
         public static string conStr = AccommodateServer.conStr;
+        //用户端公告主界面
+        public static DataTable BulletinInfo(string text)
+        {
+            string[] keywords = text.Split(',', ' ', '和', '&', '或', '与', '是', '.', '\\', '/');
+            DataTable dataTable = new DataTable();
+            using (OracleConnection connection = new OracleConnection(conStr))
+            {
+                connection.Open();
+
+                string query = "SELECT bulletin_id,heading,published_time " +
+                    "from bulletin where ";
+                foreach (string keyword in keywords)
+                {
+                    query += $" heading like '%{keyword}%' or";
+                }
+                if (query.EndsWith("or"))
+                    query = query.Substring(0, query.Length - 2);
+                query += " order by published_time desc";
+                OracleCommand command = new OracleCommand(query, connection);
+
+                OracleDataAdapter adapter = new OracleDataAdapter(command);
+
+                adapter.Fill(dataTable);
+
+                connection.Close();
+
+
+            }
+
+            Console.ReadLine();
+            return dataTable;
+        }
+        public static Bulletin SelectBulletin(string BID)
+        {
+            Bulletin btin = new Bulletin();
+            btin.Id = "B-1";
+            using (OracleConnection connection = new OracleConnection(conStr))
+            {
+                // 连接对象将在 using 块结束时自动关闭和释放资源
+                connection.Open();
+                OracleCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+                command.CommandText = $"select *from  bulletin where bulletin_id='{BID}'";
+                try
+                {
+                    OracleDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        // 访问每一行的数据
+                        // 其他列..
+                        btin.Heading = reader["heading"].ToString();
+                        btin.Id = reader["bulletin_id"].ToString();
+                        btin.published_date = Convert.ToDateTime(reader["published_time"]);
+                        btin.Content = reader["bulletin_contents"].ToString();
+                        btin.ReadCount = Convert.ToInt32(reader["read_count"]);
+                        btin.EmployeeID = Convert.ToInt32(reader["employee_id"]);
+                        // 执行你的逻辑操作，例如将数据存储到自定义对象中或进行其他处理
+
+                    }
+                    if (btin.Id == "B-1")
+                        throw new Exception("不存在的公告！");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    throw;
+                }
+                connection.Close();
+            }
+
+            return btin;
+        }
     }
 }
