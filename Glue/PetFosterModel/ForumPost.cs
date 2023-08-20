a3dfff24d522af88b4744580048c541b69bc3e46
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-20 15:04:00 +0800

    Add files via upload

---
+++ Glue/PetFosterModel/ForumPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Glue.PetFosterModel
+{
+    public class ForumPost
+    {
+        public string PostId { get; set; }
+        public string UserId { get; set; }
+        public string Heading { get; set; }
+        public string Content { get; set; }
+        public DateTime Post_time { get; set; }
+        public int ReadCount { get; set; }
+        public int LikeNum { get; set; }
+        public int CommentNum { get; set; }
+        //public ForumPost(int BID, int EID, string Heading, string Content, DateTime pd, int rd) { }
+        public ForumPost() { }
+    }
+}
