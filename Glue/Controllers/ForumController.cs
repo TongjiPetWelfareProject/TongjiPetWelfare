1470475bbc2b86bccfde99c3620de360c121043a
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-20 15:07:01 +0800

    添加返回所有帖子功能

---
+++ Glue/Controllers/ForumController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using PetFoster.BLL;
+using Oracle.ManagedDataAccess.Client;
+using System.Data;
+using System.Data.OracleClient;
+using PetFoster.DAL;
+using PetFoster.Model;
+using System.Text.Json;
+using System.Text;
+using System.Security.Cryptography;
+using System;
+using System.Numerics;
+using Glue.PetFosterModel;
+
+namespace Glue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ForumController : Controller
+    {
+        [HttpGet]
+        public IActionResult Post()
+        {
+            List<ForumPost> postlist = ForumPostManager.GetAllPosts();
+            Console.WriteLine("收到帖子请求");
+            return Ok(postlist);
+        }
+    }
+}
