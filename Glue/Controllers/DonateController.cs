e1d77f29b632240e4531a39aede9677dbcdbe247
LuminousWanderer <126951839+shiguangbiyi@users.noreply.github.com>
2023-08-21 17:03:08 +0800

    添加捐赠接口

---
+++ Glue/Controllers/DonateController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PetFoster.BLL;
+using static Glue.Controllers.RegisterController;
+
+namespace Glue.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DonateController : Controller
+    {
+        public class DonateModel
+        {
+            public string UserId { get; set; }
+            public decimal Amount { get; set; }
+
+            public DonateModel()
+            {
+                UserId = string.Empty;
+                Amount = 0;
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Donate([FromBody] DonateModel donateModel)
+        {
+            string userId = donateModel.UserId;
+            decimal amount = donateModel.Amount;
+            int status = DonationManager.Donate(userId, amount);
+            if(status == 0) { return Ok("捐款成功"); }
+            else if(status == -1) { return Unauthorized("金额信息不正确"); }
+            else if(status == -2) { return Unauthorized("金额过大，请慎重考虑"); }
+            else { return BadRequest(); }
+        }
+    }
+}
