0f7fd86806ca104354ebf85e5835ae8f03d1f71b
asceking <geyifei_king@sina.com>
2023-08-29 21:46:44 +0800

    新增：管理员医疗的完成和延期接口。注意：上传时，前端未更新，'/api/approve-medical-record'前端写成了'/api/delete-medical-record'，以及延期'/api/postpone-medical-application'中，未传递newReserveTime调整后的预约时间

--- Glue/Controllers/ManageTreatmentController.cs
+++ Glue/Controllers/ManageTreatmentController.cs
@@ -19,7 +19,7 @@ namespace Glue.Controllers
                 DataTable dt = AppointmentManager.ShowApplies();
 
                 string jsondata = ConvertTools.DataTableToJson(dt);
-                Console.WriteLine(jsondata);
+                //Console.WriteLine(jsondata);
                 return Content(jsondata, "application/json");
             }
             catch (Exception ex)
@@ -35,13 +35,102 @@ namespace Glue.Controllers
         {
             return "value";
         }
-
-        // POST api/<ManageTreatmentController>
-        [HttpPost]
-        public void Post([FromBody] string value)
+        */
+        public class MedicalRecord
         {
+            public string? pet { get; set; }
+            public string? vet { get; set; }
+            public string? reserveTime { get; set; } // 原来的预约时间
+        }
+        public class PostPoneRecord: MedicalRecord
+        {
+            public string? newReserveTime { get; set; } //选择延期到这个时间
+        }
+        // POST api/<ManageTreatmentController>
+        // 完成医疗
+        [HttpPost("approve-medical-record")]
+        public IActionResult PostApprove([FromBody] MedicalRecord record)
+        {
+            if(record == null)
+            {
+                return BadRequest("Empty Data.");
+            }
+            if(record.pet == null || !int.TryParse(record.pet, out int pid))
+            {
+                return BadRequest("Invalid Pet Id.");
+            }
+            if(record.vet == null || !int.TryParse(record.vet, out int vid))
+            {
+                return BadRequest("Invalid Vet ID.");
+            }
+            // 转换时间为DateTime格式
+            DateTime? time = ConvertTools.StringConvertToDate(record.reserveTime);
+            if(time == null)
+            {
+                //时间为空或格式错误，直接返回
+                return BadRequest("Invalid DateTime Format.");
+            }
+            // 调试
+            Console.WriteLine("pid:" + pid);
+            Console.WriteLine("vid:" + vid);
+            Console.WriteLine("time:" + record.reserveTime);
+            try
+            {
+                // logic:完成并结束医疗
+                return Ok();
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
+        // POST api/<ManageTreatmentController>
+        // 延期
+        [HttpPost("postpone-medical-application")]
+        public IActionResult PostPostpone([FromBody] PostPoneRecord record)
+        {
+            if (record == null)
+            {
+                return BadRequest("Empty Data.");
+            }
+            if (record.pet == null || !int.TryParse(record.pet, out int pid))
+            {
+                return BadRequest("Invalid Pet Id.");
+            }
+            if (record.vet == null || !int.TryParse(record.vet, out int vid))
+            {
+                return BadRequest("Invalid Vet ID.");
+            }
+            // 转换时间为DateTime格式
+            DateTime? origin_time = ConvertTools.StringConvertToDate(record.reserveTime);
+            if (origin_time == null)
+            {
+                //时间为空或格式错误，直接返回
+                return BadRequest("reserveTime: Invalid DateTime Format.");
+            }
+            DateTime? postpone_time = ConvertTools.StringConvertToDate(record.newReserveTime);
+            if (postpone_time == null)
+            {
+                //时间为空或格式错误，直接返回
+                return BadRequest("postponeTime: Invalid DateTime Format.");
+            }
+            // 调试
+            Console.WriteLine("pid:" + pid);
+            Console.WriteLine("vid:" + vid);
+            Console.WriteLine("origin_time:" + origin_time);
+            Console.WriteLine("postpone_time:" + postpone_time);
+            try
+            {
+                // logic:延期
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+        /*
         // PUT api/<ManageTreatmentController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
